9d93fbe69db95d5bcb69fad5ffd4ced261f53e4d
Dimitar Dimitrov <112943652+MitkoVtori@users.noreply.github.com>
2023-04-18 20:31:59 +0300

    Update settings.py

--- 5.Django Templates/project/settings.py
+++ 5.Django Templates/project/settings.py
@@ -11,16 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
