a6d2356caf1d81f134a801e694ba2c63bac97d81
Dimitar Dimitrov <112943652+MitkoVtori@users.noreply.github.com>
2023-05-16 12:40:12 +0300

    Update urls.py

--- 7.Workshop/petstagram/petstagram/petstagram/urls.py
+++ 7.Workshop/petstagram/petstagram/petstagram/urls.py
@@ -10,14 +10,3 @@ urlpatterns = [
     path('pets/', include('petstagram.pets.urls')),
     path('photos/', include('petstagram.photos.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-'''
-After `startapp APP_NAME`
-
-1. Create `APP_NAME/urls.py` with empty `urlpatterns`
-2. Include `APP_NAME/urls.py` into project's urls.py
-3. Add `APP_NAME` to `INSTALLED_APPS` in settings.py
-'''
